0029feb0117435d5e895368e1cfaa8d085ced74e
Emily Vertosick <vertosie@mskcc.org>
2021-08-04 12:32:15 -0400

    package structure

---
+++ R/generate_coefs.R
@@ -0,0 +1,17 @@
+
+#' Title
+#'
+#' @param data A dataframe used to create model
+#' @param covars List of covariates to be included in model
+#' @param modeltype Type of model (logistic, survival)
+#'
+#' @return dataframe
+#' @export
+#'
+#' @examples
+#' # add example here
+generate_coefs = function(data, covars, modeltype) {
+
+
+
+}
---
+++ tests/testthat.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(nomo.utils)
+
+test_check("nomo.utils")
