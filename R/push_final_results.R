30124ca16a5818344c0b1f5501f3df25ad85f82b
vertosie <vertosie@mskcc.org>
2021-08-10 16:31:00 -0400

    push_final_results function

---
+++ R/push_final_results.R
@@ -0,0 +1,108 @@
+#' push_final_results
+#'
+#' This function takes a source repository, a destination repository, and a list of files to be copied
+#' It commits and pushes any changes to the source repository, and then copies the files from the
+#' source repository to the destination repository. After files are copied, they are staged, committed and pushed to the
+#' destination repository.
+#'
+#' @param source_repo The file path for the git repository that is the source of the files
+#' @param dest_repo The file path for the destination git repository
+#' @param dest_subdir The name of the subdirectory into which the source files should be moved. Default is NULL
+#' @param source_files A vector/list(?) of files to be moved from the source repository to the destination repository
+#' @param commit_message Message to attach to the commits for both source and destination repository
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#'
+push_final_results <-
+  function(
+    source_repo,
+    dest_repo,
+    dest_subdir = NULL,
+    source_files,
+    commit_message = paste("source repo to destination repo", Sys.Date())
+  ) {
+
+
+    # TODO: Add checks to confirm filepathworks
+    # Convert file paths
+
+    # Convert strings to file path
+    source_repo <- file.path(source_repo)
+    dest_repo <- file.path(dest_repo)
+
+    # If files are to be copied into a sub-directory, create file path here
+    if (!is.null(dest_subdir)) {
+      dest_fullpath <- file.path(dest_repo, dest_subdir)
+    } else if (is.null(dest_subdir)) {
+      dest_fullpath <- file.path(dest_repo)
+    }
+
+    # Confirm all file paths exist
+    if (file.exists(source_repo) == FALSE) {
+
+      stop(glue('The file path for the source repository "{source_repo}" does not exist.'),
+           call. = FALSE)
+
+    } else if (file.exists(dest_repo) == FALSE) {
+
+      stop(glue('The file path for the destination repository "{dest_repo}" does not exist.'),
+           call. = FALSE)
+
+    } else if (file.exists(dest_fullpath) == FALSE) {
+
+      stop(glue('The destination repository sub-directory "{dest_fullpath}" does not exist.'),
+           call. = FALSE)
+    }
+
+    # Save out list of files with full file path
+    source_files_fullpath <-
+      purrr::map(source_files, ~ file.path(source_repo, .x))
+
+    # Confirm all files exist
+    for (i in source_files_fullpath) {
+
+      if (file.exists(i) == FALSE) {
+
+        stop(glue('A file in the source repository ("{i}") does not exist.'),
+             call. = FALSE)
+
+      }
+
+    }
+
+    # Update source repository
+
+    # Pull results from source repository to avoid merge conflicts
+    gert::git_pull(repo = source_repo)
+
+    # Stage and commit any changed or new files in source repo
+    gert::git_add(files = "*", repo = source_repo)
+    gert::git_commit(message = commit_message, repo = source_repo)
+
+    # Push results to source repository first to ensure consistency
+    gert::git_push(repo = source_repo)
+
+    # Update destination repository
+
+    # Pull results to avoid merge conflicts
+    gert::git_pull(repo = dest_repo)
+
+    # Copy results files from source repo folder to destination repo folder/subfolder
+    file.copy(
+      from = source_files_fullpath,
+      to = dest_fullpath
+    )
+
+    # Stage and commit all new files
+    gert::git_add(files = "*", repo = dest_repo)
+    gert::git_commit(message = commit_message, repo = dest_repo)
+
+    # Push files to results repo
+    gert::git_push(repo = dest_repo)
+
+    # TODO: Add some kind of confirmatory message?
+
+  }
---
+++ tests/testthat/test-push_final_results.R
@@ -0,0 +1,38 @@
+
+# TODO: Update these to be actual tests...
+# This worked...
+# push_final_results(
+#   source_repo = "C:\\Users\\vertosie\\Documents\\GitHub\\unique",
+#   dest_repo = "C:\\Users\\vertosie\\Documents\\GitHub\\results1",
+#   dest_subdir = "new_results",
+#   source_files = c("test1.csv", "results\\test2.csv")
+# )
+#
+# # What if file path is wrong?
+# push_final_results(
+#   source_repo = "C:\\Users\\vertosie\\Documents\\GitHub\\unique2",
+#   dest_repo = "C:\\Users\\vertosie\\Documents\\GitHub\\results1",
+#   dest_subdir = "new_results",
+#   source_files = c("test1.csv", "results\\test2.csv")
+# )
+#
+# push_final_results(
+#   source_repo = "C:\\Users\\vertosie\\Documents\\GitHub\\unique",
+#   dest_repo = "C:\\Users\\vertosie\\Documents\\GitHub\\results10",
+#   dest_subdir = "new_results",
+#   source_files = c("test1.csv", "results\\test2.csv")
+# )
+#
+# push_final_results(
+#   source_repo = "C:\\Users\\vertosie\\Documents\\GitHub\\unique",
+#   dest_repo = "C:\\Users\\vertosie\\Documents\\GitHub\\results1",
+#   dest_subdir = "new_results2",
+#   source_files = c("test1.csv", "results\\test2.csv")
+# )
+#
+# push_final_results(
+#   source_repo = "C:\\Users\\vertosie\\Documents\\GitHub\\unique",
+#   dest_repo = "C:\\Users\\vertosie\\Documents\\GitHub\\results1",
+#   dest_subdir = "new_results",
+#   source_files = c("test10.csv", "results\\test2.csv")
+# )
